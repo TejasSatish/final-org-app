694d1d7cbc7a2db09ce4d75fecff855da4218c30
tejas <tejassatish@gmail.com>
2023-10-30 15:05:07 +0530

    materialUI frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Navbar from './components/navbar/navbar'
-import Login from './pages/login/login'
-import Register from './pages/register/register'
-import Donate from './pages/donate/donate';
-import Receive from './pages/receive/receive';
-import { MoralisProvider } from 'react-moralis';
+
 function App() {
   return (
-    <MoralisProvider initializeOnMount={false}>
-      <div >
-        <Router>
-          <Navbar/>
-          <Routes>
-            <Route path="/" element={<Login/>}></Route>
-            <Route path="/register" element={<Register/>}></Route>
-            <Route path="/donate" element={<Donate/>}></Route>
-            <Route path="/receive" element={<Receive/>}></Route>
-            <Route path="/dashboard"></Route>
-          </Routes>
-        </Router>
-      </div>
-    </MoralisProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
